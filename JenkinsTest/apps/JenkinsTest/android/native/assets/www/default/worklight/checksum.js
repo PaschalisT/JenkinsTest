e4012349cb200b70efea46b2c446e596be97ad36
PaschalisT <tsolakidisp@gmail.com>
2015-06-03 09:41:26 +0200

    Mobile First test project

---
+++ JenkinsTest/apps/JenkinsTest/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3373233904,"date":1433317114492,"machine":"ps-macbook-pro.ams.nl.ibm.com"};
+/* Date: Wed Jun 03 09:38:34 CEST 2015 */
---
+++ JenkinsTest/apps/JenkinsTest/iphone/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1016721993,"date":1433317119779,"machine":"ps-macbook-pro.ams.nl.ibm.com"};
+/* Date: Wed Jun 03 09:38:39 CEST 2015 */
